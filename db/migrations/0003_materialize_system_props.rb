d74b4cc6dfab030ee99a0d83d7ccb8a9103ac379
Jon Raphaelson <jon@accidental.cc>
2023-08-31 11:54:03 -0600

    comments mainly

--- db/migrations/0003_materialize_system_props.rb
+++ db/migrations/0003_materialize_system_props.rb
@@ -1,6 +1,10 @@
 Sequel.migration do
   up do
     run <<~SQL
+      
+      -- hold system managed properties
+      -- some (most) of these are automatically created by trigger on resources
+
       CREATE TABLE properties_sys (
         id         INTEGER PRIMARY KEY AUTOINCREMENT,
         rid        TEXT NOT NULL references resources(id) ON DELETE CASCADE,
@@ -13,11 +17,9 @@ Sequel.migration do
       CREATE        INDEX properties_sys_rid_idx on properties_sys (rid);
       CREATE UNIQUE INDEX properties_sys_ridfqn_idx on properties_sys (rid, xmlns, xmlel);
 
-      DROP VIEW properties_all;
-      CREATE VIEW properties_all AS
-        SELECT * FROM properties_user
-        UNION
-        SELECT * FROM properties_sys;
+      -- because sqlite doesn't have materialized views,
+      -- we manage the system properties table via trigger
+      -- (no delete trigger, as properties will be deleted during the cascade)
 
       CREATE TRIGGER IF NOT EXISTS properties_sys_insert_resources 
       AFTER INSERT ON resources BEGIN
@@ -49,8 +51,20 @@ Sequel.migration do
           (NEW.id, 'DAV:', 'getlastmodified',    '[]', COALESCE(NEW.updated_at, NEW.created_at));
       END;
 
+      -- the dav view will be deleted in the next migration, 
+      -- but we can still use it to do the initial population
+
       INSERT INTO properties_sys (rid, xmlns, xmlel, xmlattrs, content) 
         SELECT rid, xmlns, xmlel, xmlattrs, content FROM properties_dav;
+
+      -- and lastly, the properties_all view should use this table, 
+      -- rather than the dav view
+
+      DROP VIEW properties_all;
+      CREATE VIEW properties_all AS
+        SELECT * FROM properties_user
+        UNION
+        SELECT * FROM properties_sys;
     SQL
   end
 
@@ -62,6 +76,8 @@ Sequel.migration do
         UNION
         SELECT * FROM properties_dav;
 
+      DROP TRIGGER properties_sys_insert_resources;
+      DROP TRIGGER properties_sys_update_resources;
       DROP TABLE properties_sys;
     SQL
   end
